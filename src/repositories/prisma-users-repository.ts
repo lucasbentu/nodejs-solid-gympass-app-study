d55e0c769c012f307258546e6f32dca9fa1518f1
lucasbentu <lucas.bento38@gmail.com>
2023-06-15 09:18:12 -0300

    chore(patterns):dependency inversion principle

--- src/repositories/prisma-users-repository.ts
+++ src/repositories/prisma-users-repository.ts
@@ -2,6 +2,11 @@ import { prisma } from '@/lib/prisma'
 import { Prisma } from '@prisma/client'
 
 export class PrismaUsersRepository {
+  async findUnique(where: Prisma.UserFindUniqueArgsBase) {
+    const user = await prisma.user.findUnique(where)
+    return user
+  }
+
   async create(data: Prisma.UserCreateInput) {
     const user = await prisma.user.create({
       data,
