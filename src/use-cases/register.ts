6598dd2cf8726e9a466503b8d8579a214d740513
lucasbentu <lucas.bento38@gmail.com>
2023-06-16 09:06:04 -0300

    chore(patterns):repository interface create

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,3 +1,4 @@
+import { UsersRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
 interface registerUseCaseRequest {
   name: string
@@ -6,16 +7,12 @@ interface registerUseCaseRequest {
 }
 
 export class RegisterUseCase {
-  constructor(private usersRepository: any) {}
+  constructor(private usersRepository: UsersRepository) {}
 
   async execute({ name, email, password }: registerUseCaseRequest) {
     const password_hash = await hash(password, 6)
 
-    const userWithSameEmail = await this.usersRepository.findUnique({
-      where: {
-        email,
-      },
-    })
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
     if (userWithSameEmail) {
       throw new Error('E-mail already exists.')
