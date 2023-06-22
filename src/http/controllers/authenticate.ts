cb01c54866dfa3a466f40f3bdb07a43a72c3da97
lucasbentu <lucas.bento38@gmail.com>
2023-06-22 08:47:13 -0300

    feat(pattern):implements factory pattern

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -1,8 +1,7 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
-import { AuthenticateUseCase } from '@/use-cases/authenticate'
 import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
+import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
 
 export async function authenticate(
   request: FastifyRequest,
@@ -16,8 +15,7 @@ export async function authenticate(
   const { email, password } = authenticateBodySchema.parse(request.body)
 
   try {
-    const prismaUsersRepository = new PrismaUsersRepository()
-    const authenticateUseCase = new AuthenticateUseCase(prismaUsersRepository)
+    const authenticateUseCase = makeAuthenticateUseCase()
 
     await authenticateUseCase.execute({
       email,
@@ -25,7 +23,7 @@ export async function authenticate(
     })
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
-      return reply.status(401).send({ message: err.message })
+      return reply.status(400).send({ message: err.message })
     }
 
     throw err
